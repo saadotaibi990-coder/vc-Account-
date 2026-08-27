eb5af20f69573af996f5a3778541c20224ba3add
saadotaibi990-coder <saadotaibi990@gmail.com>
2026-08-28 02:42:26 +0300

    Update channelId and serverId values

--- tokens.js
+++ tokens.js
@@ -1,8 +1,8 @@
 // status can be "online", "idle", "dnd", or "invisible" or "offline"
 export default [
     {
-        channelId: "1344059457747026005",
-        serverId: "1344059457046319198",
+        channelId: "1521300163774120017",
+        serverId: "1486273913489985657",
         token: process.env.token1,
         selfDeaf: false,
         autoReconnect: {
